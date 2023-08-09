920eba7dabaa95990e00dbbb725de35c86c5a3ea
VISHNUPRABHU <vishnurr1708@gmail.com>
2023-08-09 21:55:00 +0530

    Login and Signup page added

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -5,6 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
+
 export class SignupComponent {
+  arrin = ["Enter MailId", "Enter Password"];
+  arrup = ["Enter Username", "Enter MailId", "Enter Password", "Confirm Password"];
+  arr: string[] = [];
+  title = "Signup";
+  condition = true;
+
+  constructor() {
+    this.arr = this.arrup;
+  }
+
+  In() {
+    this.arr = this.arrin;
+    this.title = "Signin";
+    this.condition = false;
+  }
+
+  Up() {
+    this.arr = this.arrup;
+    this.title = "Signup";
+    this.condition = true;
+  }
 
 }
